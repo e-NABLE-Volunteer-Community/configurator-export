a7d06dad86b9b7a3fcc5a6d3b04e031386965c9c
ndrwksr <ndrwksr@gmail.com>
2022-03-04 09:16:17 -0600

    Remove missed import

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import './export';
 import fs from 'fs';
 import { ConfigService } from '@nestjs/config';
 import {
